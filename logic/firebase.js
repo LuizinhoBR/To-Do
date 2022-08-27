0b8429a0cbd0b1fa472f41784586826a7195771b
Luiz Henrique <92635014+LuizinhoBR@users.noreply.github.com>
2022-08-27 10:34:36 -0300

    Upload

---
+++ logic/firebase.js
@@ -0,0 +1,13 @@
+// insira suas credenciais aqui
+ const firebaseConfig = {
+    apiKey: "AIzaSyCWMgdUEj2V-JlWRlgx5f_fdhrMbrW3XVQ",
+    authDomain: "to-do-list-luizinho.firebaseapp.com",
+    databaseURL: "https://to-do-list-luizinho-default-rtdb.firebaseio.com",
+    projectId: "to-do-list-luizinho",
+    storageBucket: "to-do-list-luizinho.appspot.com",
+    messagingSenderId: "1020560991969",
+    appId: "1:1020560991969:web:96f2cca4c8032578d879a0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
